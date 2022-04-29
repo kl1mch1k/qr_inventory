d99e04b28cb718f3d599d9254d2d4a1aa513e48b
kl1mch1k <klimov.lenar@yandex.ru>
2022-04-29 15:44:25 +0300

    Working API

--- data/__all_models.py
+++ data/__all_models.py
@@ -1,3 +1,4 @@
 from . import objects
 from . import history
 from . import places
+from . import users
--- main.py
+++ main.py
@@ -1,22 +1,8 @@
-from flask import Flask, jsonify, make_response
-
-from data.db_session import global_init
 from data import api_blueprint
-
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
-
-
-# login_manager = LoginManager()
-# login_manager.init_app(app)
-
-@app.errorhandler(404)
-def not_found(error):
-    return make_response(jsonify({'error': 'Not found'}), 404)
+from flask_app import app
 
 
 def main():
-    global_init('db/inventory_new.db')
     app.register_blueprint(api_blueprint.blueprint)
     app.run()
 
